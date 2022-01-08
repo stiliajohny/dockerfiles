cdab3eaef0fbafb419eb2efc687590e9db2c2002
John Stilia <stilia.johny@gmail.com>
2022-01-08 16:56:25 +0000

    update code

--- common/setup.sh
+++ common/setup.sh
@@ -6,15 +6,11 @@ set -eu
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$(dirname "$0")"
 
-repo="https://github.com/HariSekhon/DevOps-Bash-tools"
-
-directory="common"
-
 packages="bash git make curl sudo tmux curl git-core gnupg locales zsh wget vim nano emacs"
 
 export DEBIAN_FRONTEND=noninteractive
 
-if type apk >/dev/null 2>&1;then
+if [ type apk >/dev/null 2>&1] ;then
     apk --no-cache add $packages
     elif type apt-get >/dev/null 2>&1; then
     apt-get update --allow-insecure-repositories
